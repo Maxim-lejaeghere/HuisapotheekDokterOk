a4d3d9c359cbb44fb6b01e4f1f2034ba2d8b3a03
PieterVanop <pieter.vanophalvens@student.odisee.be>
2021-01-11 11:32:07 +0100

    delete werkt bij medicijn, niet vergeten view aan te maken, later nog authorisatie op doen

--- HuisApotheek.Solution/HuisAppotheek.WepApp/Controllers/MedicijnController.cs
+++ HuisApotheek.Solution/HuisAppotheek.WepApp/Controllers/MedicijnController.cs
@@ -261,18 +261,62 @@ namespace HuisAppotheek.WepApp.Controllers
         }
 
         // GET: MedicijnController/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
-            return View();
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.GetAsync($"{baseUrl}/api/medicijns/{id}"))
+                    {
+                        var jsonValue = await response.Content.ReadAsStringAsync();
+
+                        Medicijn = JsonConvert.DeserializeObject<Medicijn>(jsonValue);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Fout bij het ophalen van de gegevens";
+
+                throw new Exception(ex.Message);
+            }
+            return View(Medicijn);
         }
 
         // POST: MedicijnController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<ActionResult> Delete(int id, IFormCollection collection)
         {
             try
             {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(baseUrl);
+
+                    // Initialiseren
+                    HttpResponseMessage response = new HttpResponseMessage();
+
+                    // Omzetten van het businessmodel naar json formaat
+                    var jsonValue = JsonConvert.SerializeObject(Medicijn);
+
+                    // opbouw van de url va de post api call
+                    var urlApi = $"{baseUrl}/api/medicijns";
+
+                    // Het inpakken van de data in de correcte verpakking
+                    var postData = new StringContent(jsonValue, System.Text.Encoding.UTF8, "application/json");
+
+                    // HTTP Post -> verstuur het pakje naar het correcte adres
+                    response = await client.DeleteAsync($"{urlApi}/{id}");
+
+                    // controle of het pakje correct is afgeleverd
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        ViewBag.Message = "Niet afgeleverd";
+                        return View();
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
             catch
